5d1a98f5cd42bf72301dd3681bd9d1a69464a5ec
JoPro0406 <55317573+JoPro0406@users.noreply.github.com>
2022-06-03 14:42:49 +0200

    feat: basic argument parsing and help message

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+fn print_usage() {
+    println!("usage: osucollectrs [id]")
+}
+
 fn main() {
-    println!("osucollectrs!");
+    let args: Vec<String> = std::env::args().collect();
+    let collector_id = match args.len() {
+        1 => {
+            print_usage();
+            std::process::exit(1)
+        }
+        2 => &args[1],
+        _ => {
+            println!("Too many arguments!");
+            print_usage();
+            std::process::exit(1)
+        }
+    };
+
+    if let Err(_) = &collector_id.parse::<usize>() {
+        println!("Invalid osu!collector id! has to be a number");
+        print_usage();
+        std::process::exit(1)
+    }
+
+    println!("Collector id: {}", collector_id);
 }
